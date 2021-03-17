2996c88f4f2d3b5b79357e8fa97326a18e7c3b2e
luckydela <luckydela93@gmail.com>
2021-03-17 18:32:18 +0000

    forex app dashboard

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+import Main from './components/main/Main';
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+
+const App = ()=> {
+
+const [sidebarOpen, setSidebarOpen] = useState(false);
+
+const openSidebar = () =>{
+  setSidebarOpen(true);
+}
+
+const closeSidebar = ()=>{
+  setSidebarOpen(false);
+}
+
+  return (
+    <div className="container">
+     <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+     <Main/> 
+     <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+    </div>
+  );
+}
+
+export default App;
